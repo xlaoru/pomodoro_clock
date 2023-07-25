b324c67dc4250450dbefd385cc77c2ae63422641
xlaoru <sia212007@gmail.com>
2023-07-25 11:33:30 +0300

    v1.1

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,11 @@
 import Clock from '../Clock/Clock';
-
 import Container from '@mui/material/Container';
 
 const App = () => {
   return (
     <div className='App'>
       <div className="background" />
-        <Container>
+        <Container sx={{display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100vh'}}>
           <Clock/>
         </Container>
     </div>
--- src/components/Clock/Clock.js
+++ src/components/Clock/Clock.js
@@ -1,13 +1,19 @@
 import React, { useState, useEffect } from 'react';
 
 const Clock = () => {
+  const categories = [{title: 'pomodoro', time: (25 * 60)}, {title: 'short break', time: (5 * 60)}, {title: 'long break', time: (15 * 60)}]
+
+  const [activeCategory, setActive] = useState()
   const [inputValue, setInputValue] = useState('');
   const [time, setTime] = useState(0);
   const [isStart, setStart] = useState(false);
   const timerRef = React.useRef(null);
 
+  const [isVisible, setVisible] = useState(false)
+
   const handleChange = (event) => {
-    setInputValue(event.target.value);
+    if(event.target.value > 99) alert('time must be less than 99 minutes')
+    else setInputValue(event.target.value);
   };
 
   const handleSubmit = (event) => {
@@ -46,21 +52,37 @@ const Clock = () => {
     };
   }, [time, isStart]);
 
+  const menuControls = categories.map((item, index) => (
+    <button 
+      disabled={isStart} 
+      key={index} 
+      className={activeCategory === index ? 'menu__item active'  : 'menu__item'}
+      onClick={() => {handleOptions(item.time); setActive(index)}}
+    >
+      {item.title}
+    </button>
+  ))
+
   return (
     <div className='Clock'>
         <div className='wrapper'>
-          <h1>{formatTimeLeft(time)}</h1>
-          <button disabled={isStart} onClick={() => handleOptions(25 * 60)}>pomodoro</button>
-          <button disabled={isStart} onClick={() => handleOptions(5 * 60)}>short break</button>
-          <button disabled={isStart} onClick={() => handleOptions(15 * 60)}>long break</button>
-          <form onSubmit={handleSubmit}>
+          <div className="menu">
+            {menuControls}
+          </div>
+          <div style={{'display': 'flex', 'justifyContent': 'center'}}>
+           <h1 className='Clock__time'>{formatTimeLeft(time)}</h1>
+          </div>
+          <form style={isVisible ? {'display': 'flex'} : {'display': 'none'}} className='form' onSubmit={handleSubmit}>
             <label>
               Enter your minutes:
-              <input disabled={isStart} type="number" value={inputValue} onChange={handleChange} />
             </label>
-            <button disabled={isStart} type="submit">Submit</button>
+            <input className='time-setter' disabled={isStart} type="number" value={inputValue} onChange={handleChange} />
+            <button disabled={isStart} type="submit" className='time-submiter'>Submit</button>
           </form>
-          <button onClick={() => time === 0 ? setStart(false) : setStart((prev) => !prev)}>Start</button>
+          <div style={{'display': 'flex', 'justifyContent': 'center', 'margin': '20px 0 5px 0'}}>
+            <button disabled={time === 0} className='form__button' style={isStart ? {'borderBottom': '2.5px solid #fff'} : {'borderBottom': '7.5px solid #fff'}} onClick={() => time === 0 ? setStart(false) : setStart((prev) => !prev)}>Start</button>
+            <button disabled={isStart} className='form__settings' onClick={() => setVisible(prev => !prev)}>Custom Time</button>
+          </div>
         </div>
     </div>
   );
